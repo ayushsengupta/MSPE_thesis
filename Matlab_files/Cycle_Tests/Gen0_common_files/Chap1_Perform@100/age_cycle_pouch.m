2347d0616c0eeea2a38f455bbe2d71aa52ab040e
Unknown <ayush.sengupta@tum.de>
2018-06-06 07:43:10 +0200

    Minor Matlab Script Changes for Gen0
    - Color coding
    - Additional ageing vs equivalent full cycle number curves

--- Matlab_files/Cycle_Tests/Gen0_common_files/Chap1_Perform@100/age_cycle_pouch.m
+++ Matlab_files/Cycle_Tests/Gen0_common_files/Chap1_Perform@100/age_cycle_pouch.m
@@ -1,7 +1,7 @@
 %% Ageing and energy throughput vs state of health evaluation
 % This fumction takes the path to a file as an input
 function [SoH,Ecn] = age_cycle_pouch(p)
-    Data = readtable(p,'Range','B5:J10','ReadRowNames',true);  
+    Data = readtable(p,'Range','B5:Z10','ReadRowNames',true);  
     Ecn = Data{'Equivalent cycle number',:}
     SoH = Data{'SoH',:}
 end
--- Matlab_files/Cycle_Tests/Gen0_common_files/Chap1_Perform@100/age_cycle_prism.m
+++ Matlab_files/Cycle_Tests/Gen0_common_files/Chap1_Perform@100/age_cycle_prism.m
@@ -1,7 +1,7 @@
 %% Ageing and energy throughput vs state of health evaluation
 % This fumction takes the path to a file as an input
 function [SoH,Ecn] = age_cycle_prism(p)
-    Data = readtable(p,'Range','B2:L8','ReadRowNames',true);  
+    Data = readtable(p,'Range','B2:Z8','ReadRowNames',true);  
     Ecn = Data{'Equivalent cycle number',:}
     SoH = Data{'SoH',:}
 end
